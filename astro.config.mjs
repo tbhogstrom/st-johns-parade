970eb776cede21b3b45eb2689b31e1567c14235d
Tyler Falcon <tbhogstrom@gmail.com>
2025-01-16 17:25:03 -0800

    fix vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: vercelServerless({
-    analytics: true,
-    includeFiles: ['**/*'],
+  adapter: vercel({
+    includeFiles: [],
+    excludeFiles: [],
     maxDuration: 8
-  }),
+  })
 });
